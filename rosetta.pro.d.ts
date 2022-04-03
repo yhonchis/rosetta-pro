6638797e7f93fb7fe475f3462da5141f72da977c
Harold Condori <yhon53190@gmail.com>
2022-04-02 19:37:21 -0500

    Cambios en la configuracion del paquete

--- rosetta.pro.d.ts
+++ rosetta.pro.d.ts
@@ -1,17 +1,15 @@
-declare module "@rosetta-pro" {
-  export interface Rosetta<T> {
-    //? Get/Set the language key
-    locale(lang?: string): string;
-    //? Define or extend the language table
-    set(lang: string, table: T): void;
-    //? Get the table of translations for a language
-    table(lang: string): T | void;
-    //? Retrieve a translation segment for the current language
-    t<X extends Record<string, any> | any[]>(
-      key: string | (string | number)[],
-      params?: X,
-      lang?: string
-    ): string;
-  }
-  export const rosetta: <T>(obj?: Record<string, T>) => Rosetta<T>;
+export interface Rosetta<T> {
+  //? Get/Set the language key
+  locale(lang?: string): string;
+  //? Define or extend the language table
+  set(lang: string, table: T): void;
+  //? Get the table of translations for a language
+  table(lang: string): T | void;
+  //? Retrieve a translation segment for the current language
+  t<X extends Record<string, any> | any[]>(
+    key: string | (string | number)[],
+    params?: X,
+    lang?: string
+  ): string;
 }
+export const rosetta: <T>(obj?: Record<string, T>) => Rosetta<T>;
